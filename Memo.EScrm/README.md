696d83c797635993ee713859854c9b5c33b44186
Memoyu <mmy6076@outlook.com>
2023-01-08 22:12:46 +0800

    style: folder structure adjustment

--- Memo.EScrm/README.md
+++
@@ -1,2 +0,0 @@
-# Memo.EScrm
- 基于企微SCRM分布式应用实现
---
+++ test/Memo.EScrm.Service.Order.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Memo.EScrm.Service.Order.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
